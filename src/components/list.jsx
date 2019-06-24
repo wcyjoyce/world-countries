b9123d0f903c0ee3e82c76be7ac9370c16a59fa3
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-06-24 08:17:58 +0800

    cleaned code

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -2,12 +2,8 @@ import React, { Component } from "react";
 
 class List extends Component {
   render() {
-
-
     return (
       <div className="list">
-
-
         {this.props.correct.map((country, index) => {
           return <li key={index}>{country}</li>
         })}
