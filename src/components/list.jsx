fee113b64e8a1c9d5a8aac9eed8ba567f04757a2
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-06-26 06:29:59 +0800

    rendering different font colours depending on input during game

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
 
 class List extends Component {
+  // style = input => {
+  //   if (this.props.time > 0) {
+  //     if (this.props.correct.includes(input)) {
+  //       return { color: "green" };
+  //     } else {
+  //       return { color: "white" };
+  //     };
+  //   } else {
+  //     if (this.props.correct.includes(input)) {
+  //       return { color: "green" };
+  //     } else {
+  //       return { color: "red" }
+  //     }
+  //   };
+  // };
+
+  style = input => {
+    if (this.props.correct.includes(input)) {
+      return "green";
+    } else {
+      return this.props.time > 0 ? "white" : "red"
+    };;
+  };
+
   render() {
     const regions = this.props.countries.map(country => country["region"]);
     const regionList = [...new Set(regions)];
@@ -13,14 +37,14 @@ class List extends Component {
 
     return (
       <div className="list">
-
         {regionTable.map(region => {
           return (
             <table key={region.region}>
               <thead><tr><th>{region.region} ({region.regionName.length})</th></tr></thead>
               <tbody>
                 {region.regionName.map((r,i) => {
-                  return <tr key={i}><td>{r["name"]}</td></tr>
+                  // return <tr key={i} style={this.style(r["name"])}><td>{r["name"]}</td></tr>
+                  return <tr key={i} style={{ color: this.style(r["name"])}}><td>{r["name"]}</td></tr>
                 })}
               </tbody>
             </table>
