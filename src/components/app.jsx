c5a47424cc3d6a3e7c29ae7f7cb83144ffcc9f4f
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-06-27 07:06:05 +0800

    proper country name added to correct list when short-form name is guessed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,7 +5,7 @@ import List from "./list.jsx";
 import countries from "./countries.js";
 
 class App extends Component {
-  state = { correct: [], countries: countries, input: "" };
+  state = { correct: [], countries: countries, input: "", time: 5, message: "" };
 
   handleChange = (event) => {
     this.setState({ input: event.target.value });
@@ -15,19 +15,40 @@ class App extends Component {
     if (event.key === "Enter") {
       if (this.state.countries.find(country => country["name"] === this.state.input || country["other"] === this.state.input)) {
         if (!this.state.correct.includes(this.state.input)) {
-          this.state.correct.push(this.state.input);
+          var answer = this.state.countries.filter(country => country["name"] === this.state.input || country["other"] === this.state.input);
+          this.state.correct.push(answer[0]["name"]);
+        } else {
+          this.setState({ message: `Already guessed! (${this.state.input})` })
         };
       };
       this.setState({ input: "" });
     };
   };
 
+  updateTime = () => {
+    this.setState({ time: this.state.time - 1 });
+  };
+
+  resetGame = () => {
+    this.setState({ correct: [], time: 5 });
+  };
+
   render() {
     return (
       <div className="app">
         <h1>World Countries</h1>
-        <Game countries={countries} handleChange={this.handleChange} handleSubmit={this.handleSubmit} correct={this.state.correct} input={this.state.input} />
-        <List countries={countries} correct={this.state.correct} />
+        <Game
+          countries={countries}
+          correct={this.state.correct}
+          input={this.state.input}
+          message={this.state.message}
+          time={this.state.time}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          updateTime={this.updateTime}
+          resetGame={this.resetGame}
+        />
+        <List countries={countries} correct={this.state.correct} time={this.state.time} />
       </div>
     );
   };
