c45906b89d6c3ddd8c7b91b67500d0c307826be6
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-07-01 00:10:21 +0800

    refactored

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,22 +1,6 @@
 import React, { Component } from "react";
 
 class List extends Component {
-  // style = input => {
-  //   if (this.props.time > 0) {
-  //     if (this.props.correct.includes(input)) {
-  //       return { color: "green" };
-  //     } else {
-  //       return { color: "white" };
-  //     };
-  //   } else {
-  //     if (this.props.correct.includes(input)) {
-  //       return { color: "green" };
-  //     } else {
-  //       return { color: "red" }
-  //     }
-  //   };
-  // };
-
   style = input => {
     if (this.props.correct.includes(input)) {
       return "green";
@@ -43,7 +27,6 @@ class List extends Component {
               <thead><tr><th>{region.region} ({region.regionName.length})</th></tr></thead>
               <tbody>
                 {region.regionName.map((r,i) => {
-                  // return <tr key={i} style={this.style(r["name"])}><td>{r["name"]}</td></tr>
                   return <tr key={i} style={{ color: this.style(r["name"])}}><td>{r["name"]}</td></tr>
                 })}
               </tbody>
